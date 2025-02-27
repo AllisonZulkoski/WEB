d4fc94c636cd73c112fc924cd3b67b27a7385d52
Allison Zulkoski <alzu1967@colorado.edu>
2025-02-27 08:54:11 -0700

    I DONT KNOW ANYMORE

--- MIT/book/book.js
+++ MIT/book/book.js
@@ -1,4 +1,4 @@
 
 function returnToPath() {
-    window.location.href = "https://allisonzulkoski.github.io/WEB/chose%20your%20path/path.html";
+    window.location.href = "https://allisonzulkoski.github.io/WEB/MIT/chose%20your%20path/path.html";
 }
---
+++ MIT/screen/screen.js
@@ -0,0 +1,12 @@
+function returnToPath() {
+    window.location.href = "https://allisonzulkoski.github.io/WEB/MIT/chose%20your%20path/path.html";
+}
+
+function leaveRoom() {
+    window.location.href = "../book/book.html";
+}
+
+function stay() {
+    // Add any functionality for staying in the room, if needed
+    alert("You chose to stay.");
+}
